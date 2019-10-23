664a6098378c71375e1b9d339a963eb1542827a0
auouymous <au@qzx.com>
2019-10-22 19:46:56 -0600

    Use make_texture for key textures.

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-function make_key(){
-	I=".key-$1.xpm"
-	O="textures/eventkeys_key_$1.png"
-	if [ "$I" -nt "$O" ]; then
-		echo "[build] $O"
-		convert $I $O
-	fi
-}
-make_key disc
-make_key gem
-make_key key
-make_key orb
-make_key star
-
 function make_texture(){
 	TYPE=$1 ; shift
 	NAME=$1 ; shift
@@ -31,6 +17,12 @@ function make_texture(){
 		montage $I -define png:exclude-chunks=date -strip -mode concatenate -tile 1x$FRAMES $* $O
 	fi
 }
+
+make_texture key disc
+make_texture key gem
+make_texture key key
+make_texture key orb
+make_texture key star
 
 make_texture node S
 make_texture node U-key 4 -blur 1x0.5
